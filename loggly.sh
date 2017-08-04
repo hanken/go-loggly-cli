f85dd5910dbae774767582bf6f8bd8c228e86d8c
Franco <franco.fallica@raisenow.com>
2017-08-04 14:03:00 +0200

    fixed loggly.sh

--- loggly.sh
+++ loggly.sh
@@ -4,6 +4,6 @@ ACCOUNT="raisenow"
 USER="raisenow"
 PASS="Bigbrother.16"
 
-CMD="docker run -it logop:dev go run loggly.go --account $ACCOUNT --user $USER --pass $PASS $@"
+CMD="docker run -it logglycli:dev go run loggly.go --account $ACCOUNT --user $USER --pass $PASS $@"
 $CMD
 
